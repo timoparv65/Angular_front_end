1bfb7e95c8aa29ec66c0cba9d98304dee045ba58
Timo Parviainen <timo_p65@yahoo.com>
2016-01-20 11:03:54 +0200

    20.1.2016 Voi etsiä paikkaa Google mapsista osoitteen peruusteella

--- FrontEnd/lib/my_script.js
+++ FrontEnd/lib/my_script.js
@@ -1,5 +1,48 @@
 $(document).ready(function(){
 	
+    // 20.1.2016 luodaan käsittelijä getBtn-painikkeelle
+    // kts: Geocoder class
+    // => https:developers.google.com/maps/documentation/javascript/reference#Geocoder
+    
+    $('#getBtn').click(function(){
+       
+        var my_address = $('#osoite').val();
+        //console.log(my_address);
+        
+        var geocoder = new google.maps.Geocoder();
+        
+        // kts. GeocoderRequest object specification
+        var geoCodeRequest = {
+            address:my_address
+        }
+        
+        geocoder.geocode(geoCodeRequest,function(response,status){
+            
+            if(status === google.maps.GeocoderStatus.OK){
+                
+                //console.log(response);
+                var data = response[0];
+                var lat = data.geometry.location.lat();
+                var lng = data.geometry.location.lng();
+                var latlng = new google.maps.LatLng(lat,lng);
+                
+                var mapProp = {
+		          center:latlng,
+		          zoom:15,
+		          mapTypeId:google.maps.MapTypeId.ROADMAP
+	           };
+                
+	           var map = new google.maps.Map(my_map,mapProp);
+                
+            }else{
+                
+                alert(status);
+            }
+            
+        });
+        
+    });
+    
 	console.log('This is ready');
     // Get location information
 	navigator.geolocation.getCurrentPosition(success, error);
